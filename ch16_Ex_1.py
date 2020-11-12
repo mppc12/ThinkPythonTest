83f2c29aeeceb8bccc8aca899e2caea666732ff0
mppc12 <mppc12@users.noreply.github.com>
2020-11-12 11:44:41 +0800

    ch16_ex1 完成

--- ch16_Ex_1.py
+++ ch16_Ex_1.py
@@ -12,24 +12,52 @@ def mul_time(time, num):
     if not valid_time(time):
         raise ValueError("請輸入正確的時間格式")
     new_time = copy.deepcopy(time)
-    seconds = time_to_int(new_time)
-    mul_sec = seconds * num
-    return int_to_time(mul_sec)
+    seconds = time_to_int(new_time)     # 轉成整數進行運算
+    mul_sec = int(seconds * num)
+    return int_to_time(mul_sec)         # 轉回時間格式，回傳新的 Time() 物件
 
 def avg_pace(time, distance):
-    """ Represent a player over """
+    """ Represent 結束比賽時每英哩所花的時間
+    time: Time object. (比賽所花的時間)
+    distance: a number (正數).
+    return: Time object. (average pace)
+    """
+    return mul_time(time, 1/distance)
+
+def mi_to_km(mi):
+    """ Represent: 英里轉公里 """
+    km = mi * 1.61
+    return km
+
+def km_to_mi(km):
+    """ Represent: 公里轉英里 """
+    mi = km / 1.61
+    return mi
 
 def main():
+    # start = Time()
+    # start.hour = 5
+    # start.minute = 10
+    # start.second = 50
 
-    start = Time()
-    start.hour = 5
-    start.minute = 10
-    start.second = 50
+    # mul_num = 5
+    # end = mul_time(start, mul_num)
+    # print_time(end)
 
-    mul_num = 5
-    end = mul_time(start, mul_num)
-    print_time(end)
-
+    game = Time()
+    game.hour = 2
+    game.minute = 5
+    game.second = 30
+    print("跑半馬所花的總時間:", end=" ")
+    print_time(game)
+    distance = 21.0975  # 半馬距離，公里
+    mi_dist = km_to_mi(distance)
+    mi_avg = avg_pace(game, mi_dist)
+    print("平均每英里所要花的時間:", end=" ")
+    print_time(mi_avg)
+    km_avg = avg_pace(game, distance)
+    print("平均每公里所要花的時間:", end=" ")
+    print_time(km_avg)
 
 
 if __name__ == "__main__":
